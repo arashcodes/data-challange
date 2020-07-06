ba0f36bf58fe453f2235e050e34a19a2a3ebfbc5
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-07-06 16:16:19 -0700

    Setup project folders and directories

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div>Hello World from App.jsx</div>
+    )
+  }
+}
+
+export default App;
