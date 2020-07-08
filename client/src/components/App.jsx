221abb4d5d4bf69d01b294b887fdf2e49054ec23
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-07-07 19:07:17 -0700

    Add child components and hard code initial data structure

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import Stats from './Stats.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      A: {total: 2540, match3: 654, match5: 439},
+      B: {total: 0, match3: 0, match5: 0},
+      C: {total: 0, match3: 0, match5: 0},
+      D: {total: 0, match3: 0, match5: 0},
+      E: {total: 0, match3: 0, match5: 0},
     }
   }
 
+
+
   render() {
     return(
-      <div>Hello World from App.jsx</div>
+      <div>
+      <Stats stats={this.state}/>
+      </div>
     )
   }
 }
