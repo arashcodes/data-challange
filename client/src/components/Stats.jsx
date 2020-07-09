436b87644e2cb16460d39fac0bdb783e64416221
Arash Abbasi <s.arash.abbasi@gmail.com>
2020-07-09 11:12:06 -0700

    Edit text in Stats and set Nodemon back to Node

--- client/src/components/Stats.jsx
+++ client/src/components/Stats.jsx
@@ -6,9 +6,9 @@ import React from 'react';
 const Stats = (props) => {
   return(
     <div>
-        <li>Rater {props.most.rater} has completed the most Task IDs of all in this time period: {props.most.total} </li>
+        <li>Rater {props.most.rater} has completed the most Task IDs of all in this time period: {props.most.total} responses to each of 3-label and 5-label types</li>
         <br />
-        <li>Rater {props.least.rater} has completed the least Task IDs of all in this time period: {props.least.total} </li>
+        <li>Rater {props.least.rater} has completed the least Task IDs of all in this time period: {props.least.total} responses to each of 3-label and 5-label types </li>
         <br />
         <li>Rater {props.highest.rater} has the overall highest agreement rate of both the  3 and 5 label answers in this time period: {props.highest.total}% </li>
         <br />
